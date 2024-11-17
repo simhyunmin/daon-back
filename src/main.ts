a1b7abc74810e5b87423233f2f5170d849875185
Sohyunxxi <dlthgus15780@gmail.com>
2024-11-17 14:28:57 +0900

    first commit

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+    'http://localhost:5000',
+    'https://port-0-daon-back-m3l3qxtx577e38d3.sel4.cloudtype.app/'
+    ],
+    credentials: true,
+  });
+  await app.listen(process.env.PORT ?? 5000);
+}
+bootstrap();
